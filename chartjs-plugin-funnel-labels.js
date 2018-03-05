f40065e5af0f5cf2531d461b092ffe3c56547e38
koopaconan <koopaconan@gmail.com>
2018-03-05 10:05:33 +0100

    Edit some changes after the merge

--- chartjs-plugin-funnel-labels.js
+++ chartjs-plugin-funnel-labels.js
@@ -12,12 +12,12 @@ Chart.plugins.register({
 		min_width_upper_label: 150,
 		allow_upper_label: true,
 		force_upper_label: false,
-    border_color: "#FFBA4B",
-    border_colors: [],
+		border_color: "#FFBA4B",
+		border_colors: [],
 		sub_text: null,
 		sub_text_style: "normal",
 		text_align: "center",
-    label_centered: true
+		label_centered: true
 	},
 
 	draw_left_label: function(chart, text, x_previous, x_current, y_current, labelIndex) {
@@ -58,63 +58,60 @@ Chart.plugins.register({
 					common_height);
 
 		// determine border color - ALLOWS GRADIENT!
-    var borderColors = this.get_option(chart, "border_colors");
-    if(borderColors.length > 1 && borderColors[labelIndex]) {
-      // render gradient
-      var borderGradient = ctx.createLinearGradient(x_rectangle, 0, x_rectangle + rectangle_width + triangle_width, 0);
-      borderGradient.addColorStop("0", borderColors[labelIndex - 1]);
-      borderGradient.addColorStop("1", borderColors[labelIndex]);
-      ctx.strokeStyle = borderGradient;
-    } else if(borderColors.length === 1) {
-      ctx.strokeStyle = borderColors[0];
-    } else {
-      // render borderColor
-      ctx.strokeStyle = this.get_option(chart, "border_color");
-    }
-    ctx.strokeRect(x_rectangle - padding_x / 2,
-      y_rectangle,
-      rectangle_width + padding_x,
-      common_height);
+		var borderColors = this.get_option(chart, "border_colors");
+		if(borderColors.length > 1 && borderColors[labelIndex]) {
+			// render gradient
+			var borderGradient = ctx.createLinearGradient(x_rectangle, 0, x_rectangle + rectangle_width + triangle_width, 0);
+			borderGradient.addColorStop("0", borderColors[labelIndex - 1]);
+			borderGradient.addColorStop("1", borderColors[labelIndex]);
+			ctx.strokeStyle = borderGradient;
+		} else if(borderColors.length === 1) {
+			ctx.strokeStyle = borderColors[0];
+		} else {
+			// render borderColor
+			ctx.strokeStyle = this.get_option(chart, "border_color");
+		}
+		ctx.strokeRect(x_rectangle - padding_x / 2,
+						y_rectangle,
+						rectangle_width + padding_x,
+						common_height);
 
 		//draw the triangle
 		ctx.beginPath();
-    ctx.lineWidth = 2;
+		ctx.lineWidth = 2;
 		ctx.moveTo(x_triangle, y_triangle);
 		ctx.lineTo(x_triangle + triangle_width, y_triangle + common_height / 2);
 		ctx.lineTo(x_triangle, y_triangle + common_height);
-    ctx.stroke();
+		ctx.stroke();
 		ctx.fill();
 
 		// draw the text
 		ctx.fillStyle = this.get_option(chart, "font_color");
-    // Calculate position of texts
-    var subText = this.get_option(chart, "sub_text");
+		// Calculate position of texts
+		var subText = this.get_option(chart, "sub_text");
 
-    var textXPosition;
-    switch(this.get_option(chart, "text_align")) {
-			case "center": {
+		var textXPosition;
+		switch(this.get_option(chart, "text_align")) {
+			case "center":
 				textXPosition = x_rectangle + rectangle_width / 2;
 				break;
-			}
-			case "right" : {
-        textXPosition = x_rectangle - rectangle_width / 2;
-        break;
-			}
-			default: {
-        textXPosition = x_rectangle;
-        break;
-			}
+			case "right" :
+				textXPosition = x_rectangle - rectangle_width / 2;
+				break;
+			default:
+				textXPosition = x_rectangle;
+				break;
 		}
-    if(subText) {
+		if(subText) {
 			var subTextSize = rectangle_width / 5;
-      ctx.fillText(text, textXPosition, y_rectangle + common_height / 2 - font_size / 2 - subTextSize / 2);
-      ctx.font = Chart.helpers.fontString(subTextSize, this.get_option(chart, "sub_text_style"), this.get_option(chart, "font_family"));
-      ctx.fillText(subText, textXPosition, y_rectangle + common_height / 2 + subTextSize / 3);
-    } else {
-      ctx.fillText(text, textXPosition, y_rectangle + common_height / 2 - font_size / 2);
-    }
+			ctx.fillText(text, textXPosition, y_rectangle + common_height / 2 - font_size / 2 - subTextSize / 2);
+			ctx.font = Chart.helpers.fontString(subTextSize, this.get_option(chart, "sub_text_style"), this.get_option(chart, "font_family"));
+			ctx.fillText(subText, textXPosition, y_rectangle + common_height / 2 + subTextSize / 3);
+		} else {
+			ctx.fillText(text, textXPosition, y_rectangle + common_height / 2 - font_size / 2);
+		}
 
-    ctx.restore();
+		ctx.restore();
 
 	},
 
